3a848cf3931b91dcf33748ce2a79746b8ea753e3
artifishional <artifishional@mail.ru>
2020-07-19 04:17:16 +0300

    - s2s draft

--- src/kit/index.js
+++ src/kit/index.js
@@ -2,21 +2,26 @@ import { stream2 as stream } from 'm2';
 
 const { random } = Math;
 
+function gen(cb, count = 100, rand = false) {
+  cb([{
+    blocks: Array
+      .from(
+        { length: rand ? random() * count + 1 | 0 : count },
+        (_, id) => ({
+          id,
+          type: ['red', 'green', 'blue'][random() * 3 | 0],
+          msg: String.fromCharCode(65 + id)
+        })
+      )
+      .sort(() => random() - 0.5)
+  }]);
+}
+
 export default () => stream
   .fromCbFunc((cb) => {
+    gen(cb);
     return setInterval(() => {
-      cb([{
-        blocks: Array
-          .from(
-            { length: random() * 100 + 1 | 0 },
-            (_, id) => ({
-              id,
-              type: ['red', 'green', 'blue'][random() * 3 | 0],
-              msg: String.fromCharCode(65 + id)
-            })
-          )
-          .sort(() => random() - 0.5)
-      }]);
+      gen(cb);
     }, 2000);
   })
   .store()
